0a94aacafee03ef52c520799bcf3a8a99c4c73ca
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2026-07-12 11:14:32 +0600

    initialize the user model

--- user/models.py
+++ user/models.py
@@ -7,13 +7,19 @@ from django.utils.translation import gettext_lazy as _
 
 from .custom_manager import CustomUserManager
 
+from utils.user_related import user_type
+
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    user_type = models.CharField(max_length=10, choices=user_type, default='customer')
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    address= models.TextField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name']
 
     objects = CustomUserManager()
 
---
+++ utils/user_related.py
@@ -0,0 +1,6 @@
+user_type = (
+    ('customer', 'Customer'),
+    ('vendor', 'Vendor'),
+    ('admin', 'Admin'),
+    
+)
